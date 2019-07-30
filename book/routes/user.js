b29a2b314a06854100df6822a2f52f40f610f995
2287963892 <www.2287963892@qq.com>
2019-07-30 08:48:28 +0800

    换血

--- book/routes/user.js
+++
@@ -1,5 +0,0 @@
-const express=require('express');
-const pool=require('./pool.js');
-var router=express.Router();
-//
-module.exports=router;
---
+++ bookapp/routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const pool=require('./pool.js');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
