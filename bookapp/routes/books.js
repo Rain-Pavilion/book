7e11c8b32f874a70b94cb49e6decb24464dc2f26
1075488066 <1075488066@qq.com>
2019-08-03 20:04:55 +0800

    写了购物车表头，添加了两个接口

--- bookapp/routes/books.js
+++ bookapp/routes/books.js
@@ -1,18 +1,28 @@
 var express = require('express');
-const pool=require('./pool.js');
+const pool = require('./pool.js');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/query', function(req, res, next) {
-   let lid=req.query.lid,
-       sql='select * from book_laptop where lid=?',
-       values=[lid];
-   pool.query(sql,values,function (error,result) {
-     if(error)throw error;
-     res.send(result);
-   })
+router.get('/query', function (req, res, next) {
+    let lid = req.query.lid,
+        sql = 'select * from book_laptop where lid=?',
+        values = [lid];
+    pool.query(sql, values, function (error, result) {
+        if (error) throw error;
+        res.send(result);
+    })
 });
 
+router.get('/cquery', function (req, res, next) {
+    let {conditions, num} = req.query;
+    console.log(conditions,num);
+    let values = [conditions, parseInt(num)],
+        sql = 'select * from book_laptop order by ? desc limit ?';
+    pool.query(sql, values, function (error, result) {
+        if (error) throw error;
+        res.send(result);
+    })
+});
 
 
 module.exports = router;
