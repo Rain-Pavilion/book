ce790422959289e08f4d48dd09856ff11b232dfc
1075488066 <1075488066@qq.com>
2019-08-07 21:51:11 +0800

    test

--- bookapp/public/js/index.js
+++ bookapp/public/js/index.js
@@ -1,54 +1,53 @@
 let url = 'http://127.0.0.1:8080/books/cquery?conditions=lid_count&num=80';
 axios.get(url).then((function (response) {
     let data = response.data;
-    let num=Math.floor(Math.random()*80);
+    let num = Math.floor(Math.random() * 80);
     let html1 = '';
-        html1 += `   <img src="${data[num].lg_pic}" alt="" >
+    html1 += `   <img src="${data[num].lg_pic}" alt="" >
         <div>
           <p>${data[num].book_name}</p>
           <p>￥${data[num].price}</p>
-          <p><del>￥${parseInt(data[num].price*1.2)}</del></p>
+          <p><del>￥${parseInt(data[num].price * 1.2)}</del></p>
           <p>${data[num].comment_count}条评论</p>
       </div>
         `;
     new_book.innerHTML = html1;
     // html1
     let html2 = '';
-    for(var i=0;i<=7;i++){
+    for (var i = 0; i <= 7; i++) {
         html2 += `
           <div class="laptop">
           <a href="/detail.html?lid=${data[i].lid}"><img src="${data[i].lg_pic}" alt=""></a>
           <p><a href="/detail.html?lid=${data[i].lid}" title="${data[i].book_name}">${data[i].book_name}</a></p>
           <p title="${data[i].publishing}">${data[i].publishing}</p>
-          <p><span>￥${data[i].price}</span><span><del>￥${parseInt(data[i].price*1.2)}</del></span></p>
-          <p><span>电子书￥${parseInt(data[i].price*0.5)}</span></p>
+          <p><span>￥${data[i].price}</span><span><del>￥${parseInt(data[i].price * 1.2)}</del></span></p>
+          <p><span>电子书￥${parseInt(data[i].price * 0.5)}</span></p>
         </div>
-        `; 
+        `;
     }
     top_middle_title.innerHTML = html2;
     //middle_laptop1
-    let html_laptop1=``;
-    for(var j=1;j<=3;j++){
-      if(j==1){
-        var span="新";
-        var p="书推荐";
-        var title="新书热卖榜";
-      }else if(j==2){
-        var span="当";
-        var p="当推荐";
-        var title="近7日畅销榜";
-      }
-      else if(j==3){
-        var span="畅";
-        var p="销";
-        var title="电子书榜";
-      }
-    html_laptop1+=`
+    let html_laptop1 = ``;
+    for (var j = 1; j <= 3; j++) {
+        if (j == 1) {
+            var span = "新";
+            var p = "书推荐";
+            var title = "新书热卖榜";
+        } else if (j == 2) {
+            var span = "当";
+            var p = "当推荐";
+            var title = "近7日畅销榜";
+        } else if (j == 3) {
+            var span = "畅";
+            var p = "销";
+            var title = "电子书榜";
+        }
+        html_laptop1 += `
     <div class="middle">
     <div class="banner">
-    <img src="img/index/banner${(j-1)*3+1}.jpg" alt="">
-    <img src="img/index/banner${(j-1)*3+2}.jpg" alt="">
-    <img src="img/index/banner${(j-1)*3+3}.jpg" alt="">
+    <img src="img/index/banner${(j - 1) * 3 + 1}.jpg" alt="">
+    <img src="img/index/banner${(j - 1) * 3 + 2}.jpg" alt="">
+    <img src="img/index/banner${(j - 1) * 3 + 3}.jpg" alt="">
   </div>
   <div class="top_middle_title clearFloat">
       <span>${span}</span><p>${p} <a href="">更多新书戳这里</a></p>
@@ -63,50 +62,41 @@ axios.get(url).then((function (response) {
       </div>
               
   </div>
-  <div class="middle_list">
-    <ul id="middle_li${j}">
-      
-    </ul>
-  </div>
-
-  </div>
     `;
-  }
-  middle_laptop1.innerHTML=html_laptop1;
+    }
+    middle_laptop1.innerHTML = html_laptop1;
     // html3
     // midddle left
-    for(let k=1;k<=3;k++){
-      let html_middle=``;
-      if(k==1){
-        var start=8;
-        var end=17;
-      }else if(k==2){
-        var start=18;
-        var end=27;
-      }else if(k==3){
-        var start=28;
-        var end=32;
-      }
-    for(var i=start;i<=end;i++){
-      html_middle+=`
+    for (let k = 1; k <= 3; k++) {
+        let html_middle = ``;
+        if (k == 1) {
+            var start = 8;
+            var end = 17;
+        } else if (k == 2) {
+            var start = 18;
+            var end = 27;
+        } else if (k == 3) {
+            var start = 28;
+            var end = 32;
+        }
+        for (var i = start; i <= end; i++) {
+            html_middle += `
       <div class="laptop">
           <a href="/detail.html?lid=${data[i].lid}"><img src="${data[i].lg_pic}" alt=""></a>
           <p class="name"><a href="/detail.html?lid=${data[i].lid}" title="${data[i].book_name}">${data[i].book_name}</a></p>
           <p class="author" title="}">${data[i].author}</p>
           <p class="time"><span>${data[i].shelf_time}</span></p>
-          <p class="price"><span>￥${data[i].price}</span><span><del>￥${parseInt(data[i].price*1.2)}</del></span></p> 
+          <p class="price"><span>￥${data[i].price}</span><span><del>￥${parseInt(data[i].price * 1.2)}</del></span></p> 
         </div>
-        `; 
+        `;
+        }
+        if (k == 1) {
+            new_laptop1.innerHTML = html_middle
+        } else if (k == 2) {
+            new_laptop2.innerHTML = html_middle
+        } else {
+            new_laptop3.innerHTML = html_middle
+        }
+
     }
-    if(k==1){
-      new_laptop1.innerHTML=html_middle
-    }else if(k==2){
-      new_laptop2.innerHTML=html_middle
-    }else{
-      new_laptop3.innerHTML=html_middle
-    }
-    
-  }
-    
-    
 }));
