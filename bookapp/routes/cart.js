511575cbfd7ed7a0ad6d8e3c69fc8a06f23cc684
1075488066 <1075488066@qq.com>
2019-08-30 17:08:08 +0800

    cart

--- bookapp/routes/cart.js
+++ bookapp/routes/cart.js
@@ -3,7 +3,7 @@ const pool = require('./pool.js');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/query', function (req, res, next) {
+router.get('/queryCart', function (req, res, next) {
     let iid = req.query.iid,
         values=[iid],
         sql = 'select * from book_shoppingcart_item where iid=?';
@@ -13,14 +13,14 @@ router.get('/query', function (req, res, next) {
     })
 });
 
-router.get('/add',function (req,res,next) {
+router.get('/addCart',function (req,res,next) {
     let lid=req.query.lid,
-        uid=req.query.uid,
+        uid=req.session.uid;
         obj={
             user_id:uid,
             product_id:lid,
-        },
-        values=[obj],
+        };
+        values=[obj];
         sql='insert into book_shoppingcart_item set ?';
     pool.query(sql,values,function (error, result) {
         if(error)throw error;
@@ -28,10 +28,5 @@ router.get('/add',function (req,res,next) {
         res.send(info);
     })
 });
-
-router.get('/query',function (req,res,next) {
-
-});
-
 
 module.exports = router;
